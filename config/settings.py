f138051fcb95f3d0871d5c126d1dfa3f28455141
math0917 <math0917@naver.com>
2022-08-08 14:33:54 +0900

    몰라 합칠거얌 잠시만

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFIELS_DIRS = [
+    BASE_DIR/ 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
