4f9cdff4afa845b257f1e6ea34f1f30d807b1f35
math0917 <math0917@naver.com>
2022-08-08 19:40:34 +0900

    login finish

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,15 @@ STATICFIELS_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+AUTHENTICATION_BACKENDS = (
+    
+    # Needed to login by username in Django admin, regardless of `allauth` 
+    'django.contrib.auth.backends.ModelBackend',
+ 
+    # `allauth` specific authentication methods, such as login by e-mail 
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+)
 
 AUTH_USER_MODEL = "accounts.User"
 ACCOUNT_SIGNUP_REDIRECT_URL = "index"
