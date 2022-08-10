49ed4d137c7e22fe9b91e24a63d89afb82cf3c24
Seojisoo20191941 <amy0480@naver.com>
2022-08-10 18:31:01 +0900

    a

--- dot/views.py
+++ dot/views.py
@@ -9,18 +9,3 @@ def cherry(request):
 
 def input(request):
     return render(request, 'input.html')
-
-
-def output(request, id):
-    # if 'user_email' not in request.session:
-    #     return redirect('login_view')
-    # widget = get_object_or_404(StoreWidget, seq=id)
-    # related_widgets=StoreWidget.objects.filter(widget_type=widget.widget_type).exclude(seq=id)
-    # email= request.session['user_email']
-    # user = User_info.objects.get(user_email=email)
-
-    # # comment = get_object_or_404(Comment, id=id)
-    # comments = Comment.objects.filter(widget=widget)
-    date = get_object_or_404(date, id=id)
-
-    return render(request, 'output.html', {"date": date, 'title': title})
